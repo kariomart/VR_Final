ffb66f6e7ceadb2e87aff6a8238a9428b4689d08
Unknown <mnn264@nyu.edu>
2019-04-25 10:16:58 -0400

    weee

---
+++ Assets/BPMController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioHelm;
+
+public class BPMController : MonoBehaviour
+{
+
+    public AudioHelmClock clock;
+    public Vector2 defaultPos;
+    int defaultBPM = 120;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     defaultPos = transform.position;   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dif = transform.position.y - defaultPos.y;
+        clock.bpm = 120 + dif;
+    }
+}
--- Assets/SynthController.cs
+++ Assets/SynthController.cs
@@ -13,6 +13,8 @@ public class SynthController : MonoBehaviour
 
     public int numKeys = 60;
     public int startingKey = 24;
+    public float rangeOfMovementX;
+    public float rangeOfMovementY;
 
     GroundKey[] keys;
     int currentNote;
@@ -32,6 +34,8 @@ public class SynthController : MonoBehaviour
             NoteOn(currentNote);
         }
 
+        SetFilter();
+
     }
 
     void NoteOn(int index)
@@ -47,5 +51,29 @@ public class SynthController : MonoBehaviour
         int octave = index / scale.Length;
         int noteInScale = index % scale.Length;
         return startingKey + octave * octaveSize + scale[noteInScale];
+    }
+
+    public void SetFilter()
+    {
+        float val = hand1.transform.position.x;
+        val = Remap(val, -10f, 10f, 0, 1);  
+        val = Mathf.Clamp(val, 0, 1);
+        synth.SetParameterAtIndex(0, val);
+        synth.SetParameterAtIndex(1, 1-val);
+    }
+
+    public float Remap (float from, float fromMin, float fromMax, float toMin,  float toMax)
+    {
+        var fromAbs  =  from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;      
+       
+        var normal = fromAbs / fromMaxAbs;
+ 
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+ 
+        var to = toAbs + toMin;
+       
+        return to;
     }
 }
