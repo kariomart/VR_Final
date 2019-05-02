e6084f855af0b4229edfce40cac649f44ac691d1
Unknown <mnn264@nyu.edu>
2019-05-02 11:30:15 -0400

    lots of fixes

---
+++ Assets/AudioShaderReact.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is used to adjust how much our mesh changes with audio input
+//now that we have a MIDI controller, we want to have all our controls on the same object
+public class AudioShaderReact : MonoBehaviour {
+
+	//assign the spectrum analyzer that you wish to control material properties with
+    public SpectrumAnalysis analyzer;
+	
+	//lower values = smoother animation
+	public float smoothing = 0.75f;
+
+	[UnityEngine.Range(0f, 1f)] public float shaderTimeAmount;
+	[UnityEngine.Range(50f, 20000f)] public float shaderTimeLowBound, shaderTimeHighBound;
+
+	[UnityEngine.Range(0f, 10f)] public float shaderDisplaceAmount;
+	
+	[UnityEngine.Range(0f, 100f)] public float shaderColorAmount;
+	[UnityEngine.Range(50f, 20000f)] public float shaderColorLowBound, shaderColorHighBound;
+	
+	public GameObject reactiveGameObject;
+	[SerializeField] Material myMaterial;
+	
+	
+    float normalizedAudioInput = 0f;
+	float prevAudioInput = 0f;
+	float smoothedAudioInput = 0f;
+
+	private float prevShaderTime;
+	private float prevShaderColor;
+	
+
+	// Use this for initialization
+	void Start () {
+
+		myMaterial = reactiveGameObject.GetComponent<Renderer> ().material;
+		analyzer = GetComponent<SpectrumAnalysis>();
+		prevShaderTime = 0f;
+		prevShaderColor = 0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		//we use the overall audio input to adjust the displacement
+		normalizedAudioInput = analyzer.GetWholeEnergy() * shaderDisplaceAmount;
+
+		//we're applying some additional smoothing here
+		smoothedAudioInput = Mathf.Lerp (prevAudioInput, normalizedAudioInput, smoothing);
+		prevAudioInput = smoothedAudioInput;
+
+		//Find the time value from the range of frequencies that we want
+		float shaderTime = analyzer.GetEnergyFrequencyRange(shaderTimeLowBound, shaderTimeHighBound);
+		shaderTime *= shaderTimeAmount;
+		
+		//Apply Smoothing
+		shaderTime = Mathf.Lerp(prevShaderTime, shaderTime, smoothing);
+		prevShaderTime = shaderTime;
+		
+		float shaderColor = analyzer.GetEnergyFrequencyRange(shaderColorLowBound, shaderColorHighBound);
+		shaderColor *= shaderColorAmount;
+
+		
+		shaderColor = Mathf.Lerp(prevShaderColor, shaderColor, smoothing);
+		prevShaderColor = shaderColor;
+
+		//check to see if we have a material, then start adjusting shader values
+		if (myMaterial != null) {
+			myMaterial.SetFloat ("_AudioInput", smoothedAudioInput);
+			myMaterial.SetFloat ("_ShaderTime", shaderTime);
+			myMaterial.SetFloat ("_ShaderColor", shaderColor);
+		}
+        
+	}
+}
--- Assets/SynthController.cs
+++ Assets/SynthController.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using AudioHelm;
+using OVR;
 
 public class SynthController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public bool left;
+    public bool vrEnabled;
+    public AudioSource source;
     public HelmController synth;
     public Transform hand;
     public Vector2 defaultPos;
+
     public int[] scale = {0, 2, 4, 5, 7, 9, 11 };
     public int octaveSize = 12;
+    public int minNote;
+    public int octaveSpan;
 
     public int numKeys = 60;
     public int startingKey = 24;
+
     public float rangeOfMovementX;
     public float rangeOfMovementY;
 
@@ -23,8 +31,12 @@ public class SynthController : MonoBehaviour
     GroundKey[] keys;
     int currentNote;
 
+    Vector2 leftThumbstickVal;
+    Vector2 rightThumbstickVal;
+
     void Start()
     {
+        source = GetComponent<AudioSource>();
         defaultPos = hand.position; 
 
     }
@@ -32,13 +44,29 @@ public class SynthController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float diff = hand.position.y - defaultPos.y;
-        diff = Remap(diff, -rangeOfMovementY, rangeOfMovementY, -10, 10);
-        int tempNote = Mathf.RoundToInt(noteMin + diff);
+       //UpdateRangeOfMotion();
+        float diffY = hand.position.y - defaultPos.y;
+        diffY = Remap(diffY, -rangeOfMovementY, rangeOfMovementY, -10, 10);
+
+        float diffX = hand.position.x - defaultPos.x;
+        source.panStereo = getPan(diffX);
+
+        int tempNote = Mathf.RoundToInt(noteMin + diffY);
+
         if (tempNote != currentNote) {
+            ChordOff(currentNote);
             currentNote = tempNote;
-            NoteOn(currentNote);
-            Debug.Log(diff + " " + currentNote);
+            ChordOn(currentNote);
+            Debug.Log(diffY + " " + currentNote);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            synth.AllNotesOff();
+            synth.NoteOn(60, 1f);
+        }
+
+        if (vrEnabled) {
+            VRInput();
         }
 
         SetFilter();
@@ -53,6 +81,32 @@ public class SynthController : MonoBehaviour
         //keys[index].SetOn(true);
     }
 
+    void NoteOff(int index)
+    {
+        if (synth) {
+            synth.NoteOff(GetKeyFromIndex(index));
+        }
+
+    }
+
+    void ChordOn(int index) {
+
+        if (synth) {
+            synth.NoteOn(index);
+            synth.NoteOn(index+4);
+            synth.NoteOn(index+7);
+        }
+    }
+
+    void ChordOff(int index) {
+
+        if (synth) {
+            synth.NoteOff(index);
+            synth.NoteOff(index+4);
+            synth.NoteOff(index+7);
+        }
+    }
+
     int GetKeyFromIndex(int index)
     {
         int octave = index / scale.Length;
@@ -60,6 +114,19 @@ public class SynthController : MonoBehaviour
         return startingKey + octave * octaveSize + scale[noteInScale];
     }
 
+    // int GetNote(int index) {
+
+    //     int note;
+    //     note = minNote + scale[Random.Range(0,scale.Length)] + (12*Random.Range(0, octaveSpan));
+    // }
+
+    public float getPan(float val) {
+
+        float pan =  Remap(val, -rangeOfMovementX, rangeOfMovementX, -.8f, .8f);
+        pan = Mathf.Clamp(pan, -.8f, .8f);
+        return pan;
+
+    }
     public void SetFilter()
     {
         float val = hand.transform.position.x;
@@ -67,6 +134,47 @@ public class SynthController : MonoBehaviour
         val = Mathf.Clamp(val, 0, 1);
         synth.SetParameterAtIndex(0, .5f+val);
         synth.SetParameterAtIndex(1, 1-val);
+        
+        if (vrEnabled) {
+            if (left) {
+                synth.SetParameterAtIndex(4, leftThumbstickVal.x);
+                synth.SetParameterAtIndex(4, leftThumbstickVal.y);
+            } else {
+                synth.SetParameterAtIndex(5, rightThumbstickVal.x);
+                synth.SetParameterAtIndex(5, rightThumbstickVal.y);
+            }
+        }
+    }
+
+    public void SetParameter(int index, float val) {
+        synth.SetParameterAtIndex(index, val);
+    }
+
+    public void UpdateRangeOfMotion() {
+
+        if (hand.position.x > rangeOfMovementX) {
+            rangeOfMovementX = hand.position.x;
+        }
+
+        if (hand.position.y > rangeOfMovementY) {
+            rangeOfMovementY = hand.position.y;
+        }
+
+
+    }
+
+    void VRInput() {
+
+        if (left) {
+            source.volume=OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger);
+            Vector2 leftThumbstickVal = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        } else {
+            source.volume=OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);  
+            Vector2 rightThumbstickVal = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        }
+
+
+
     }
 
     public float Remap (float from, float fromMin, float fromMax, float toMin,  float toMax)
